a170cdd3ba0625c3eed80cd345a948d878ba6866
Argen96 <106479453+Argen96@users.noreply.github.com>
2022-11-29 15:25:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
 import fsp from 'fs/promises';
 
 async function uniqueValue(file){
- const importedFile= await fsp.readFile(file,'utf-8')
+ const importedFile = await fsp.readFile(file,'utf-8');
  const array = importedFile.split('\r\n');
  console.log(array)
- const uniqueArray= new Set(array);
+ const uniqueArray = new Set(array);
  return uniqueArray;
 }
 
